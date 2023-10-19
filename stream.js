72fc98f30f9afd814013dc55d6e0816641ef5d17
cbm7920 <sarginmert@gmail.com>
2023-10-19 19:59:30 +0200

    design

--- stream.js
+++ stream.js
@@ -3,13 +3,22 @@ const { exec } = require('child_process');
 function livestreamToTwitch() {
     const cmd = 'ffmpeg -stream_loop -1 -i ./src/GnomeMP4.mp4 -stream_loop -1 -i ./src/song.wav -c:v copy -c:a aac -shortest -f flv rtmp://live.twitch.tv/app/live_973039533_9BXRQV7EQVAoGo45Rg5KfDWtnd3ucj';
 
-    exec(cmd, (error, stdout, stderr) => {
-        if (error) {
-            console.error(`exec error: ${error}`);
-            return;
-        }
-        console.log(`stdout: ${stdout}`);
-        console.error(`stderr: ${stderr}`);
+    const child = exec(cmd);
+
+    child.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+    });
+
+    child.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+    });
+
+    child.on('error', (error) => {
+        console.error(`exec error: ${error}`);
+    });
+
+    child.on('close', (code) => {
+        console.log(`child process exited with code ${code}`);
     });
 }
 
