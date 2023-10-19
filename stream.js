0e50f5809c82ed749ab6c7fe55638261afb381a4
cbm7920 <sarginmert@gmail.com>
2023-10-19 20:04:01 +0200

    design

--- stream.js
+++ stream.js
@@ -1,25 +1,31 @@
 const { exec } = require('child_process');
+require('dotenv').config();
 
 function livestreamToTwitch() {
-    const cmd = 'ffmpeg -stream_loop -1 -i ./src/GnomeMP4.mp4 -stream_loop -1 -i ./src/song.wav -c:v copy -c:a aac -shortest -f flv rtmp://live.twitch.tv/app/live_973039533_9BXRQV7EQVAoGo45Rg5KfDWtnd3ucj';
+    console.log("Starting the livestreamToTwitch function...");
+
+    const cmd = `ffmpeg -stream_loop -1 -i ./src/GnomeMP4.mp4 -stream_loop -1 -i ./src/song.wav -c:v copy -c:a aac -shortest -f flv ${process.env.TWITCH_STREAM_URL}`;
+    console.log(`Executing command: ${cmd}`);
 
     const child = exec(cmd);
 
     child.stdout.on('data', (data) => {
-        console.log(`stdout: ${data}`);
+        console.log(`[FFMPEG STDOUT]: ${data}`);
     });
 
     child.stderr.on('data', (data) => {
-        console.error(`stderr: ${data}`);
+        console.error(`[FFMPEG STDERR]: ${data}`);
     });
 
     child.on('error', (error) => {
-        console.error(`exec error: ${error}`);
+        console.error(`Child process error: ${error}`);
     });
 
     child.on('close', (code) => {
-        console.log(`child process exited with code ${code}`);
+        console.log(`Child process exited with code ${code}`);
     });
 }
 
+console.log("Starting the script...");
 livestreamToTwitch();
+console.log("Finished initializing the livestream function (it will run in the background).");
