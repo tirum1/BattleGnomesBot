8ae39653d8a0fcf06e5a6bddd9649003a59026df
cbm7920 <sarginmert@gmail.com>
2023-10-21 23:34:01 +0200

    design

--- GnomesBattleBot.js
+++ GnomesBattleBot.js
@@ -22,7 +22,7 @@ bot.onText(/\/?nft ([\d,]+)/i, async (msg, match) => {
     const userId = msg.from.id;
     const currentTime = Date.now();
     if (userTimestamps[userId] && (currentTime - userTimestamps[userId] < RATE_LIMIT)) {
-        bot.sendMessage(msg.chat.id, "Please wait 10 seconds before using a command again. Alternatively, you can use /nft with up to 10 IDs at once (e.g., /nft ID1, ID2, ...).");
+        bot.sendMessage(msg.chat.id, `${safeUsername} Please wait 10 seconds before using a command again. Alternatively, you can use /nft with up to 10 IDs at once (e.g., /nft ID1, ID2, ...).`);
         return;
     }
     userTimestamps[userId] = currentTime;
@@ -81,6 +81,10 @@ bot.onText(/\/?nft ([\d,]+)/i, async (msg, match) => {
 
 bot.onText(/\/?leaderboard/i, async (msg) => {
     const chatId = msg.chat.id;
+    
+    // Extract username or first name from the message sender
+    const username = msg.from.username ? `@${msg.from.username}` : msg.from.first_name;
+    const safeUsername = username.replace(/_/g, '\\_'); // Escape underscores for Markdown
   
     try {
         const aliveArray = await battleContract.getAliveByID();
@@ -96,11 +100,11 @@ bot.onText(/\/?leaderboard/i, async (msg) => {
         
         const top30 = sortedAliveArray.slice(0, 30);
 
-        let responseTitle = '';
+        let responseTitle = `${safeUsername}, `;
         if (top30.length <= 5) {
-            responseTitle = '*WINNERS OF LAST GAME*\n\n';
+            responseTitle += '*WINNERS OF LAST GAME*\n\n';
         } else {
-            responseTitle = '*TOP 30 ALIVE NFTs*\n\n';
+            responseTitle += '*TOP 30 ALIVE NFTs*\n\n';
         }
 
         const medals = ['🥇', '🥈', '🥉'];
@@ -113,19 +117,23 @@ bot.onText(/\/?leaderboard/i, async (msg) => {
                 : `${index + 1}. ID: ${id} - Game Wins: ${roundWins}`;
         }).join('\n');
         
-  
         bot.sendMessage(chatId, responseTitle + response, { parse_mode: 'Markdown' });
   
     } catch (error) {
-        bot.sendMessage(chatId, 'Error fetching leaderboard. Please try again later.');
+        bot.sendMessage(chatId, `${safeUsername}, Error fetching leaderboard. Please try again later.`, { parse_mode: 'Markdown' });
         console.error(error);
     }
 });
 
-bot.onText(/\/?ca/i, (msg) => {
-    let replyText = '';
 
-    replyText += "*Smart Contract Addresses:*\n\n";
+bot.onText(/\/?ca/i, (msg) => {
+    const chatId = msg.chat.id;
+    
+    // Extract username or first name from the message sender
+    const username = msg.from.username ? `@${msg.from.username}` : msg.from.first_name;
+    const safeUsername = username.replace(/_/g, '\\_'); // Escape underscores for Markdown
+  
+    let replyText = `${safeUsername}, here are the *Smart Contract Addresses:*\n\n`;
     
     replyText += "🛡️ *Battle Contract:*\n";
     replyText += `\`${battleContractAddress}\`\n\n`;
@@ -136,91 +144,43 @@ bot.onText(/\/?ca/i, (msg) => {
     replyText += "🎨 *NFT Contract:*\n";
     replyText += `\`${GnomesCollectiveAddress}\`\n`;
   
-    bot.sendMessage(msg.chat.id, replyText, { parse_mode: 'Markdown' });
+    bot.sendMessage(chatId, replyText, { parse_mode: 'Markdown' });
 });
+
 
 bot.onText(/\/?time/i, async (msg) => {
     const userId = msg.from.id;
     const currentTime = Date.now();
+
+    // Extract username or first name from the message sender
+    const username = msg.from.username ? `@${msg.from.username}` : msg.from.first_name;
+    const safeUsername = username.replace(/_/g, '\\_'); // Escape underscores for Markdown
+
     if (userTimestamps[userId] && (currentTime - userTimestamps[userId] < RATE_LIMIT)) {
-        bot.sendMessage(msg.chat.id, "Please wait for 10 seconds before using a command again.");
+        bot.sendMessage(msg.chat.id, `${safeUsername}, Please wait for 10 seconds before using a command again.`);
         return;
     }
     userTimestamps[userId] = currentTime;
     const chatId = msg.chat.id;
 
-  try {
-      console.log("Fetching game state from the contract...");
+    try {
+        console.log("Fetching game state from the contract...");
 
-      // Fetching game state from the contract
-      const newGame = await battleContract.newGame();
-      console.log("newGame:", newGame);
+        // ... (rest of your code remains unchanged)
+        
+        // Modify bot sendMessage calls to include the safeUsername
+        bot.sendMessage(chatId, `${safeUsername}, ${notificationMessage + `${minutes}:${seconds.toString().padStart(2, '0')} minutes!`}`, { parse_mode: 'Markdown' });
 
-      const HungerGamesBegin = await battleContract.HungerGamesBegin();
-      console.log("HungerGamesBegin:", HungerGamesBegin);
-
-      const timerPassed = await battleContract.hasTimerPassed();
-      console.log("timerPassed:", timerPassed);
-
-      const roundsCount = await battleContract.roundsCount();
-      console.log("roundsCount:", roundsCount);
-
-      const currentTime = Math.floor(Date.now() / 1000);
-      console.log("currentTime:", currentTime);
-
-      const startTimer = await battleContract._startTimer();
-      console.log("startTimer:", startTimer);
-
-      let intervalTime = await battleContract.roundDuration();
-      console.log("intervalTime:", intervalTime);
-
-      let notificationMessage;
-
-      const roundsCountNum = roundsCount.toNumber();  // I assume you meant to convert 'roundsCount' to a number first before adding 1
-      console.log("Converted roundsCount:", roundsCountNum);
-      
-      const startTimerNum = startTimer.toNumber();
-      console.log("Converted startTimer:", startTimerNum);
-      
-      let intervalTimeNum = intervalTime.toNumber();
-      console.log("Converted intervalTime:", intervalTimeNum);
-      
-      if (newGame) {
-        intervalTimeNum = intervalTimeNum * 6;
-          notificationMessage = `🚀 New Hunger Games will begin in `;
-          console.log("Setting interval for new game:", intervalTimeNum);
-      } else if (!newGame && HungerGamesBegin) {
-          notificationMessage = `⏱️ The next round will begin in `;
-          console.log("Setting interval for next round:", intervalTime);
-      }
-      console.log("Converted new intervalTime:", intervalTimeNum);
-      const remainingTime = startTimerNum + intervalTimeNum - currentTime;
-      console.log("remainingTime:", remainingTime);
-
-      const minutes = Math.floor(remainingTime / 60);
-      const seconds = remainingTime % 60;
-      console.log("Computed minutes and seconds:", minutes, seconds);
-
-      // Sending appropriate messages based on game state
-      if (timerPassed) {
-          bot.sendMessage(chatId, `🚀 The timer has passed! The Hunger Games or round has begun or will begin shortly!`);
-      } else if (remainingTime > 0) {
-          bot.sendMessage(chatId, notificationMessage + `${minutes}:${seconds.toString().padStart(2, '0')} minutes!`);
-      } else {
-          bot.sendMessage(chatId, `🚀 The Hunger Games or round has begun!`);
-      }
-
-      if (newGame) {
-        bot.sendMessage(chatId, `Currently, in round 0.`);
-    } else {
-        bot.sendMessage(chatId, `Currently, in round ${roundsCountNum}.`);
-    }
+        if (newGame) {
+            bot.sendMessage(chatId, `${safeUsername}, Currently, in round 0.`);
+        } else {
+            bot.sendMessage(chatId, `${safeUsername}, Currently, in round ${roundsCountNum}.`);
+        }
     
-      
-  } catch (error) {
-      bot.sendMessage(chatId, 'Error fetching game timing. Please try again later.');
-      console.error("Error in the /time command:", error);
-  }
+    } catch (error) {
+        bot.sendMessage(chatId, `${safeUsername}, Error fetching game timing. Please try again later.`);
+        console.error("Error in the /time command:", error);
+    }
 });
 
 bot.onText(/\/?stats ([\d,]+)/i, async (msg, match) => {
