c786ea43b7f2d3941277c4c3cea748ba5f7b9e8a
cbm7920 <sarginmert@gmail.com>
2023-11-05 00:01:16 +0100

    design

--- ColloseumBot.js
+++ ColloseumBot.js
@@ -747,7 +747,7 @@ function startBot() {
             const totalNFTs = ownerNFTs.length;
             const queuedNFTs = [];
             let queuedCount = 0;
-            let balance = ownerBalanceBIG.toString();
+            let balance =  parseFloat(ownerBalanceBIG.toString());
             
             for (let i = 1; i <= totalNFTs; i++) {
                 const tokensRequired = minBalanceRequiredBIG.add(
