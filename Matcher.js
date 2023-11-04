3215c8f09534c887dcc655a7c5c780fe35cc5080
cbm7920 <sarginmert@gmail.com>
2023-11-05 00:35:09 +0100

    design

--- Matcher.js
+++ Matcher.js
@@ -282,12 +282,12 @@ async function editMessageViaAxios(chatId, messageId, newText) {
     }
 }
 function hasTimerPassed() {
-    if (newGame) {
+    if(balance >= 0.25){
+        return true
+    } else if (newGame) {
         return Math.floor(Date.now() / 1000) >= (time + roundDuration * 3); 
     } else if (HungerGamesBegin) {
         return Math.floor(Date.now() / 1000) >= (time + roundDuration);
-    } else if(balance >= 0.25) {
-        return true;
     } else {
         return false;
     }
